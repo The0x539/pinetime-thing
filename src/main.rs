233a67c4fd2a092e0fc2b0768bdb582573a97850
The0x539 <the0x539@gmail.com>
2024-02-17 19:01:13 -0600

    actually copy the files to the watch

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,30 @@
+use std::time::SystemTime;
+
 use btleplug::api::{Central, CentralEvent, Manager as _, Peripheral as _, ScanFilter};
 use btleplug::platform::Manager;
 use btleplug::Result;
 use futures::StreamExt;
 
 use infinitime_resources::InfiniTime;
+use serde::Deserialize;
 
 #[tokio::main(flavor = "current_thread")]
-async fn main() -> Result<()> {
+async fn main() -> anyhow::Result<()> {
     let mut watch = get_infinitime().await?.unwrap();
 
-    let version = watch.version().await?;
-    println!("version {version}");
+    std::env::set_current_dir("../../../Desktop/resources")?;
 
-    let entries = watch.list_dir("/").await?;
-    for e in entries {
-        println!("{e:?}");
+    let resources: Resources = serde_json::from_slice(&std::fs::read("./resources.json")?)?;
+
+    watch.create_dir("/fonts", SystemTime::now()).await?;
+    watch.create_dir("/images", SystemTime::now()).await?;
+
+    for resource in resources.resources {
+        let data = std::fs::read(&resource.filename)?;
+        println!("Transferring {} to {}", resource.filename, resource.path);
+        let now = SystemTime::now();
+        watch.write_file(&resource.path, &data, now).await?;
+        println!("Done!");
     }
 
     Ok(())
@@ -53,4 +63,25 @@ async fn get_infinitime() -> Result<Option<InfiniTime>> {
     }
 
     Ok(None)
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct Resources {
+    pub resources: Vec<Resource>,
+    pub obsolete_files: Vec<ObsoleteFile>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct Resource {
+    pub filename: String,
+    pub path: String,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct ObsoleteFile {
+    pub path: String,
+    pub since: String,
 }
